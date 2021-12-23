9e0da269054bd56c1c8216a6e2b243fa80b42712
Alex Freitas <freitas.alex.soares@outlook.com>
2021-12-22 22:38:06 -0300

    PersonServiceImplementation

---
+++ API_REST_ASPNET/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using API_REST_ASPNET.Model;
+using System.Collections.Generic;
+
+namespace API_REST_ASPNET.Services
+{
+    interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person>FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
